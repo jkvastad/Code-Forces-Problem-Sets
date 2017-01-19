7c446738033053798b12f69532d1b85969f5c4bf
Johan Kvastad <jkvastad@gmail.com>
2017-01-19 15:22:58 +0100

    Added a Javascript folder for javascript solutions. Added Java solution for problem 1A.

--- Java/src/_1A/Main.java
+++ Java/src/_1A/Main.java
@@ -1,11 +1,21 @@
 package _1A;
 
+import java.util.Scanner;
+
 /**
  * Created by Johan on 1/19/2017.
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
-
+        Scanner scanner = new Scanner(System.in);
+        long n,m,a;
+        n = scanner.nextLong();
+        m = scanner.nextLong();
+        a = scanner.nextLong();
+        long vertical_stones = n/a;
+        if (n%a>0){vertical_stones++;}
+        long horizontal_stones = m/a;
+        if(m%a>0){horizontal_stones++;}
+        System.out.println(vertical_stones*horizontal_stones);
     }
 }
